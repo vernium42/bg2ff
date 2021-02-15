429de94f197716090261b478ca3fbdf945e9c5e3
Victor Verdun van der Watt <vernium@icloud.com>
2021-02-15 07:52:13 +0200

    Fixing pip install bug

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bq2ff
 Save query from Bigquery to a flatfile
+
+## How to install bq2ff
+```shell
+pip3 install git+https://github.com/vernium42/bq2ff.git
+```
--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools
 
-setup(name='bq2ff',
-      version='0.1',
+setuptools.setup(name='bq2ff',
+      version='0.2',
       description='Bigquery to flatfile',
       url='https://github.com/vernium42/bq2ff',
       author='Vernium42',
       license='MIT',
-      packages=['bq2ff', 'pandas', 'pyarrow', 'google-cloud-bigquery'])
+      packages=setuptools.find_packages())
