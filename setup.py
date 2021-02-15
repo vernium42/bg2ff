04df53fd218744f2bf1e15a29e88efe20570682e
Victor Verdun van der Watt <vernium@icloud.com>
2021-02-15 08:04:48 +0200

    Added requirements file and updated setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import os
 import setuptools
+
+folder_path = os.path.dirname(os.path.realpath(__file__))
+requirements = folder_path + '/requirements.txt'
+with open(requirements) as f:
+      requires = f.read().splitlines()
 
 setuptools.setup(name='bq2ff',
       version='0.2',
@@ -6,4 +12,5 @@ setuptools.setup(name='bq2ff',
       url='https://github.com/vernium42/bq2ff',
       author='Vernium42',
       license='MIT',
+      install_requires=requires,
       packages=setuptools.find_packages())
